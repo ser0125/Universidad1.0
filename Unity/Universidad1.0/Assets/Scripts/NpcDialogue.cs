0666ec5c7b1f3768d978cf87e84d4308ac3cda8b
ser0125 <sergio.llanos.garcia@gmail.com>
2016-11-10 07:52:03 -0500

    first commit

---
+++ Unity/Universidad1.0/Assets/Scripts/NpcDialogue.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NpcDialogue : MonoBehaviour {
+
+	public string[] answers;
+	public string[] Questions;
+	bool displayDialogue=false;
+	public GameObject character1;
+	bool isClicked;
+
+	void OnTriggerEnter(){
+		displayDialogue = true;
+	
+
+	}
+
+	void OnTriggerExit(){
+		displayDialogue = false;
+	}
+
+	void OnGUI(){
+
+		GUILayout.BeginHorizontal(); 
+		bool isHorizontalBlockActive = true; 
+		if (displayDialogue) {
+			
+			GUILayout.Label ( Questions [0]);
+			GUILayout.Label ( Questions [1]);
+
+			isClicked=GUILayout.Button (answers [0]);
+	
+			if (isClicked) {
+				Debug.Log ("aquii");
+				Invoke ("WaitToEnd",5f);
+	
+			}
+			GUILayout.Button ( answers [1]);
+			isHorizontalBlockActive = false;     
+
+			displayDialogue = false;
+			if(isHorizontalBlockActive)
+				GUILayout.EndHorizontal();
+			
+	
+	}
+
+
+
+}
+}
---
+++ Unity/Universidad1.0/Assets/Scripts/Play.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour {
+
+	 void OnMouseDown() {
+		if(Input.GetMouseButtonDown(0)){
+			SceneManager.LoadScene ("Game");
+		}
+	}
+
+}
